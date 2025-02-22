4b6bc854fd8fd803ea6cb4514a5a299ecb62272b
alexyan1 <alexryanyan@gmail.com>
2025-02-22 14:53:45 -0800

    convert 24 to 00 datetime

--- data.py
+++ data.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 # Download latest version
 path = kagglehub.dataset_download("stoney71/new-york-city-transport-statistics")
-
 print("Path to dataset files:", path)
 
 print("Files in the directory:", os.listdir(path))
@@ -14,15 +13,20 @@ csv_file = os.path.join(path, 'mta_1706.csv')  # Adjust file name accordingly
 df = pd.read_csv(csv_file, on_bad_lines='skip')
 
 print(df.head())
-"""
+
 df = pd.read_csv('mta_1706.csv', on_bad_lines= 'skip')
 
 
 # Drop rows with missing values in the critical columns
 df = df.dropna(subset=['ScheduledArrivalTime', 'RecordedAtTime'])
 
-# Convert scheduled/actual arrival times to datetime format
-df['ScheduledArrivalTime'] = pd.to_datetime(df['ScheduledArrivalTime'])
+# Assuming your 'ScheduledArrivalTime' column has invalid '24:00' times
+df['ScheduledArrivalTime'] = df['ScheduledArrivalTime'].replace(to_replace="24:00", value="00:00")
+df['ScheduledArrivalTime'] = df['ScheduledArrivalTime'].replace(to_replace="25:00", value="01:00")
+
+# Convert the 'ScheduledArrivalTime' to datetime after replacement
+df['ScheduledArrivalTime'] = pd.to_datetime(df['ScheduledArrivalTime'], errors='coerce')
+
 df['RecordedAtTime'] = pd.to_datetime(df['RecordedAtTime'])
 
 # Calculate delay in minutes
@@ -36,4 +40,3 @@ df['IsWeekend'] = df['DayOfWeek'].isin([5, 6]).astype(int)  # 5, 6 represent Sat
 print(df.head(3))
 
 # print(df.columns)
-"""
